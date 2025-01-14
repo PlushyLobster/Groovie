edaf6a7579d0e98f945b6553b0f7551d54532174
Thomas PARESCHI <thomas.pareschi@campus-centre.fr>
2025-01-14 18:35:38 +0100

    insertion des données de test pour les festivals et les genres musicaux

--- database/migrations/202511_create_partner_table.php
+++ database/migrations/202511_create_partner_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
         Schema::create('GRV1_Partner', function (Blueprint $table) {
             $table->id('Id_partner');
             $table->string('name', 100);
-            $table->string('logo', 100);
             $table->timestamps();
         });
     }
