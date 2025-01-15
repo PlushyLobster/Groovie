ead201e34072b6dadb31f0adcb5bac850552ef09
ItsCastoor <thomas.pareschi@hotmail.com>
2025-01-15 14:06:27 +0100

    Maj de la BDD et regénération du nouveau jeu de données

---
+++ database/migrations/202519_create_tickets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('GRV1_Tickets', function (Blueprint $table) {
+            $table->id('Id_ticket');
+            $table->string('number', 50);
+            $table->dateTime('date_of_use');
+            $table->timestamps();
+            $table->foreignId('Id_user')->constrained('GRV1_Users')->references('Id_user')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('Id_festival')->constrained('GRV1_Festivals')->references('Id_festival')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('GRV1_Tickets');
+    }
+};
