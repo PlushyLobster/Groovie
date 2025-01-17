bcb1b9b76e124e14c38adbebac5adfa4d3c84f88
Théo Jorge Oliveira <theo.joliveira@gmail.com>
2025-01-17 11:05:03 +0100

    animation modales connexion et inscription

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -60,7 +60,6 @@
 
 @section('scripts')
 <script>
-    // Gestion du dropdown principal (toggleDropdownButton)
     const signInBtn = document.getElementById('signIn-btn');
     const signinDropdown = document.getElementById('signinDropdown');
     const logInBtn = document.getElementById("logIn-btn");
@@ -68,25 +67,35 @@
 
     if (signInBtn && signinDropdown && logInBtn && loginDropdown) {
         signInBtn.addEventListener('click', (event) => {
-            event.stopPropagation(); // Empêche le clic de remonter
-            loginDropdown.classList.add('hidden'); // Masque le dropdown de connexion
-            signinDropdown.classList.toggle('hidden'); // Affiche ou masque le menu d'inscription
+            event.stopPropagation();
+            loginDropdown.classList.add('hidden');
+            signinDropdown.classList.remove('hidden');
             signinDropdown.classList.add('flex');
+            signinDropdown.classList.add('transition-right');
         });
 
         logInBtn.addEventListener("click", (event) => {
-            event.stopPropagation(); // Empêche le clic de remonter
-            signinDropdown.classList.add('hidden'); // Masque le menu d'inscription
-            loginDropdown.classList.toggle("hidden"); // Affiche ou masque le dropdown de connexion
+            event.stopPropagation();
+            signinDropdown.classList.add('hidden');
+            loginDropdown.classList.remove("hidden");
             loginDropdown.classList.add("flex");
+            loginDropdown.classList.add("transition-right-login");
         });
 
         document.addEventListener('click', (event) => {
             if (!signinDropdown.contains(event.target) && event.target !== signInBtn) {
-                signinDropdown.classList.add('hidden'); // Masque le menu d'inscription si clic en dehors
+                signinDropdown.classList.add('transition-backRight');
+                setTimeout(() => {
+                    signinDropdown.classList.add('hidden');
+                    signinDropdown.classList.remove('transition-backRight');
+                }, 800);
             }
             if (!loginDropdown.contains(event.target) && event.target !== logInBtn) {
-                loginDropdown.classList.add('hidden'); // Masque le dropdown de connexion si clic en dehors
+                loginDropdown.classList.add('transition-backRight-login');
+                setTimeout(() => {
+                    loginDropdown.classList.add('hidden'); 
+                    loginDropdown.classList.remove('transition-backRight-login');
+                }, 800);
             }
         });
 
