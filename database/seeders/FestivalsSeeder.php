4d167a84c935a4624e752bf55b4ef2c6846edcee
ItsCastoor <thomas.pareschi@hotmail.com>
2025-01-23 11:51:37 +0100

    Feat: préparation des images

--- database/seeders/FestivalsSeeder.php
+++ database/seeders/FestivalsSeeder.php
@@ -4,33 +4,93 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Faker\Factory as Faker;
 
 class FestivalsSeeder extends Seeder
 {
     public function run(): void
     {
-        $faker = Faker::create();
-
-        $existingNames = DB::table('GRV1_Festivals')->pluck('name')->toArray();
-        $festivals = [];
-
-        for ($i = 0; $i < 10; $i++) {
-            do {
-                $name = $faker->company;
-            } while (in_array($name, $existingNames));
-
-            $existingNames[] = $name;
-
-            $festivals[] = [
-                'type' => $faker->randomElement(['Intérieur', 'Extérieur']),
-                'name' => $name,
-                'start_datetime' => $faker->dateTimeBetween('2024-01-01', '2024-12-31'),
-                'end_datetime' => $faker->dateTimeBetween('2024-01-01', '2024-12-31'),
+        $festivals = [
+            [
+                'type' => 'Extérieur',
+                'name' => 'Festival de Cannes',
+                'start_datetime' => '2025-05-14 10:00:00',
+                'end_datetime' => '2025-05-25 23:59:00',
                 'created_at' => now(),
                 'updated_at' => now(),
-            ];
-        }
+            ],
+            [
+                'type' => 'Intérieur',
+                'name' => 'Printemps de Bourges',
+                'start_datetime' => '2025-04-22 15:00:00',
+                'end_datetime' => '2025-04-27 23:59:00',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'type' => 'Extérieur',
+                'name' => 'Les Vieilles Charrues',
+                'start_datetime' => '2025-07-18 12:00:00',
+                'end_datetime' => '2025-07-21 23:59:00',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'type' => 'Extérieur',
+                'name' => 'Rock en Seine',
+                'start_datetime' => '2025-08-22 14:00:00',
+                'end_datetime' => '2025-08-25 23:00:00',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'type' => 'Intérieur',
+                'name' => 'Festival Lumière',
+                'start_datetime' => '2025-10-12 18:00:00',
+                'end_datetime' => '2025-10-20 23:59:00',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'type' => 'Extérieur',
+                'name' => 'Fête de la Musique',
+                'start_datetime' => '2025-06-21 10:00:00',
+                'end_datetime' => '2025-06-21 23:59:00',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'type' => 'Intérieur',
+                'name' => 'Hellfest',
+                'start_datetime' => '2025-06-20 15:00:00',
+                'end_datetime' => '2025-06-23 02:00:00',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'type' => 'Extérieur',
+                'name' => 'Jazz à Vienne',
+                'start_datetime' => '2025-06-28 17:00:00',
+                'end_datetime' => '2025-07-13 23:59:00',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'type' => 'Extérieur',
+                'name' => 'Festival d\'Avignon',
+                'start_datetime' => '2025-07-04 09:00:00',
+                'end_datetime' => '2025-07-26 23:59:00',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'type' => 'Intérieur',
+                'name' => 'Nuits Sonores',
+                'start_datetime' => '2025-05-28 20:00:00',
+                'end_datetime' => '2025-06-01 23:59:00',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
 
         DB::table('GRV1_Festivals')->insert($festivals);
     }
